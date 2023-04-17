699599f1269a444025a5c666cc9143ba10b9fb53
Saswata Banerjee <azure.saswata@gmail.com>
2023-04-17 12:04:03 +0530

    2021 events moved to one html page

--- js/mainmenu.js
+++ js/mainmenu.js
@@ -1,4 +1,30 @@
 
 $(function () {
-    $('#mainmenu').html('<nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Tenth navbar example"><div class= "container-fluid" ><button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span></button><div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08"><ul class="navbar-nav">  <li class="nav-item"><a class="nav-link active" aria-current="page" href="address-list.html">ADDRESS LIST</a>  </li>  <li class="nav-item"> <a class="nav-link active" aria-current="page" href="/jgm2023/">Jhargram</a>  </li> <li class="nav-item"> <a class="nav-link disabled">Paschimbanga</a>  </li>  <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"     aria-expanded="false">2023</a> <ul class="dropdown-menu">     <li><a class="dropdown-item" href="2023apr.html">April</a></li>     <li><a class="dropdown-item" href="#march">March</a></li>     <li><a class="dropdown-item" href="#february">February</a></li> </ul>  </li>  <li class="nav-item"> <a class="nav-link" href="#2022">2022</a>  </li>  <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"     aria-expanded="false">2021</a> <ul class="dropdown-menu">     <li><a class="dropdown-item" href="#apr">April</a></li>     <li><a class="dropdown-item" href="2021mar.html">March</a></li>     <li><a class="dropdown-item" href="#february">February</a></li> </ul>  </li>  <li class="nav-item"> <a class="nav-link" href="#2020">2020</a>  </li>  <li class="nav-item"> <a class="nav-link" href="#">2019</a>  </li>  <li class="nav-item"> <a class="nav-link" href="#">2018</a>  </li>  <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"     aria-expanded="false">More</a> <ul class="dropdown-menu">     <li><a class="dropdown-item" href="#bangalore">Bangalore</a></li>     <li><a class="dropdown-item" href="#delhi">Delhi</a></li>     <li><a class="dropdown-item" href="#mumbai">Mumbai</a></li> </ul>  </li> </ul></div>                </div >            </nav >');
+    $("#mainmenu").html(`<nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Tenth navbar example">
+      <div class= "container-fluid" >
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation"> 
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+          <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link active" aria-current="page" href="address-list.html">ADDRESS LIST</a>  </li>  
+            <li class="nav-item"> <a class="nav-link active" aria-current="page" href="/jgm2023/">Jhargram</a>  </li> 
+            <li class="nav-item"> <a class="nav-link disabled">Paschimbanga</a>  </li>  
+            <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"     aria-expanded="false">2023</a> 
+              <ul class="dropdown-menu">     
+                <li><a class="dropdown-item" href="2023apr.html">April</a></li>     
+                <li><a class="dropdown-item" href="#march">March</a></li>     
+                <li><a class="dropdown-item" href="#february">February</a></li> 
+              </ul>
+            </li>  
+            <li class="nav-item"> <a class="nav-link" href="#2022">2022</a>  </li>
+            <li class="nav-item"> <a class="nav-link" href="2021.html">2021</a>  </li>  
+            <li class="nav-item"> <a class="nav-link" href="#2020">2020</a>  </li>  
+            <li class="nav-item"> <a class="nav-link" href="#">2019</a>  </li>  
+            <li class="nav-item"> <a class="nav-link" href="#">2018</a>  </li>  
+            <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"     aria-expanded="false">More</a> <ul class="dropdown-menu">     <li><a class="dropdown-item" href="#bangalore">Bangalore</a></li>     <li><a class="dropdown-item" href="#delhi">Delhi</a></li>     <li><a class="dropdown-item" href="#mumbai">Mumbai</a></li> </ul>  </li> 
+          </ul>
+        </div>                
+      </div >            
+    </nav >`);
     })
